83eb1adec7f347d3e4e2e20bf4dc090c0bb980ba
slackers123 <srekcals@proton.me>
2025-07-31 15:25:54 +0200

    Simple Map and search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,31 @@
 pub type FieldID = u16;
 
-#[derive(Clone, Default, Debug)]
-pub struct Field {
-    pub id : FieldID,
-
-    pub taxi_conn : Vec<FieldID>,
-
-    pub bus_conn : Option<Vec<FieldID>>,
-    pub metro_conn : Option<Vec<FieldID>>,
-    pub ferry_conn : Option<Vec<FieldID>>
-}
-
-impl Field {
-    pub fn new(id : FieldID) -> Self {
-        Self {
-            id,
-            ..Default::default()
-        }
-    }
-
-    pub fn has_bus(&self) -> bool {
-        self.bus_conn.is_some()
-    }
-
-    pub fn has_metro(&self) -> bool {
-        self.metro_conn.is_some()
-    }
+pub enum ConnectionType {
+    Taxi,
+    Bus,
+    Underground,
+    Ferry,
 }
 
 pub struct Map {
-    pub field_list : Vec<Field>
+    taxi_connections: Vec<(FieldID, FieldID)>,
+    bus_connections: Vec<(FieldID, FieldID)>,
+    underground_connections: Vec<(FieldID, FieldID)>,
+    ferry_connections: Vec<(FieldID, FieldID)>,
 }
 
 impl Map {
-    pub fn add_field(&mut self) -> &mut Field {
-        self.field_list.push(Field::new(self.field_list.len() as FieldID)); 
-        self.field_list.last_mut().unwrap()  
+    pub fn possible_targets(&self, field: FieldID, ty: ConnectionType) -> Vec<FieldID> {
+        let to_search = match ty {
+            ConnectionType::Taxi => &self.taxi_connections,
+            ConnectionType::Bus => &self.bus_connections,
+            ConnectionType::Underground => &self.underground_connections,
+            ConnectionType::Ferry => &self.ferry_connections,
+        };
+
+        to_search
+            .iter()
+            .filter_map(|(a, b)| (*a == field).then(|| *b).or((*b == field).then(|| *a)))
+            .collect()
     }
 }
